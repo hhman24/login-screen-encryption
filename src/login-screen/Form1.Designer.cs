963a72bdd449d06fdf0c4274cd331184a45e5ad0
hhman24 <anhoang483@gmail.comC:/Users/hhman/AppData/Local/Programs/Python/Python39/python.exe>
2023-05-31 00:20:30 +0700

    hhman - .NET C#

---
+++ src/login-screen/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace login_screen
+{
+    partial class login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(login));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.panel_image1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.checkBoxPas = new System.Windows.Forms.CheckBox();
+            this.ExitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // imageList2
+            // 
+            this.imageList2.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList2.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // panel_image1
+            // 
+            this.panel_image1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel_image1.BackgroundImage")));
+            this.panel_image1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_image1.Location = new System.Drawing.Point(0, 0);
+            this.panel_image1.Name = "panel_image1";
+            this.panel_image1.Size = new System.Drawing.Size(317, 230);
+            this.panel_image1.TabIndex = 0;
+            this.panel_image1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_image1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 255);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // username
+            // 
+            this.username.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(233)))));
+            this.username.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.username.Font = new System.Drawing.Font("MS UI Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.username.Location = new System.Drawing.Point(35, 286);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(254, 33);
+            this.username.TabIndex = 2;
+            this.username.TextChanged += new System.EventHandler(this.username_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 335);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // password
+            // 
+            this.password.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(233)))));
+            this.password.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.password.Font = new System.Drawing.Font("MS UI Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password.Location = new System.Drawing.Point(35, 361);
+            this.password.Name = "password";
+            this.password.PasswordChar = '*';
+            this.password.Size = new System.Drawing.Size(254, 33);
+            this.password.TabIndex = 4;
+            this.password.TabStop = false;
+            this.password.TextChanged += new System.EventHandler(this.password_TextChanged);
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.Color.RoyalBlue;
+            this.loginButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.loginButton.FlatAppearance.BorderSize = 0;
+            this.loginButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.loginButton.Font = new System.Drawing.Font("Nirmala UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.ForeColor = System.Drawing.Color.White;
+            this.loginButton.Location = new System.Drawing.Point(35, 446);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(254, 33);
+            this.loginButton.TabIndex = 5;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = false;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // checkBoxPas
+            // 
+            this.checkBoxPas.AutoSize = true;
+            this.checkBoxPas.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkBoxPas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBoxPas.Location = new System.Drawing.Point(138, 400);
+            this.checkBoxPas.Name = "checkBoxPas";
+            this.checkBoxPas.Size = new System.Drawing.Size(151, 27);
+            this.checkBoxPas.TabIndex = 6;
+            this.checkBoxPas.Text = "Show Password";
+            this.checkBoxPas.UseVisualStyleBackColor = true;
+            this.checkBoxPas.CheckedChanged += new System.EventHandler(this.checkBoxPas_CheckedChanged);
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.BackColor = System.Drawing.Color.White;
+            this.ExitBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ExitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ExitBtn.Font = new System.Drawing.Font("Nirmala UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExitBtn.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.ExitBtn.Location = new System.Drawing.Point(34, 485);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(254, 33);
+            this.ExitBtn.TabIndex = 7;
+            this.ExitBtn.Text = "Exit";
+            this.ExitBtn.UseVisualStyleBackColor = false;
+            this.ExitBtn.Click += new System.EventHandler(this.ExitBtn_Click_1);
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(317, 544);
+            this.Controls.Add(this.ExitBtn);
+            this.Controls.Add(this.checkBoxPas);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel_image1);
+            this.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(165)))), ((int)(((byte)(169)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.Panel panel_image1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.CheckBox checkBoxPas;
+        private System.Windows.Forms.Button ExitBtn;
+    }
+}
+
---
+++ src/login-screen/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Diagnostics.Tracing;
+
+namespace login_screen
+{
+    public partial class login : Form
+    {
+        // Tạo kết nối global
+        SqlConnection conn = new SqlConnection(@"Data Source=PC-AN;Initial Catalog=QLSinhVien;Integrated Security=True");
+
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        /// 
+        /// Connect database 
+        ///
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //
+        // Test box
+        //
+        private void username_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void password_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //
+        // Button
+        //
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+
+
+
+            string _user = username.Text.Trim();
+            Console.WriteLine("USER NAME: " + _user);
+            // truong hop ly neu chuoi la varchar, nvarchar
+            // choi la ki tu co dau
+            // string co luu duoc unicode ???
+            string _password = password.Text.Trim();
+            Console.WriteLine("PASS WORD: "+ _password);
+
+            string querry =
+                "IF(" +
+                "   EXISTS( SELECT SV.MASV" +
+                "           FROM SINHVIEN SV" +
+                "           WHERE SV.TENDN LIKE N'" + _user + "' AND SV.MATKHAU = HASHBYTES('MD5', N'" + _password + "'))" +
+                "   OR" +
+                "   EXISTS( SELECT NV.MANV" +
+                "           FROM NHANVIEN NV" +
+                "           WHERE NV.TENDN LIKE N'" + _user + "' AND NV.MATKHAU = HASHBYTES('SHA1', N'" + _password + "'))) " +
+                "SELECT 1 ELSE SELECT 0";
+
+            try
+            {
+                conn.Open();
+
+                Console.WriteLine(querry);
+                SqlCommand cmd = new SqlCommand(querry, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                if(reader.Read() == true)
+                {
+                    if (reader.GetValue(0).ToString() == "1")
+                    {
+                        MessageBox.Show("Login Success!", "Login", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        username.Text = "";
+                        password.Text = "";
+                        username.Focus();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failed to Login!", "Login", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        username.Text = "";
+                        password.Text = "";
+                        username.Focus();
+                    }
+                }
+
+                reader.Close();
+                cmd.Dispose();
+                conn.Close();
+            }
+            catch(Exception err)
+            {
+                Console.WriteLine(err);
+                MessageBox.Show("Cannot connect to DB!", "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                conn.Close();
+            }
+        }
+
+        private void ExitBtn_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        // Check box
+        private void checkBoxPas_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxPas.Checked)
+            {
+                password.PasswordChar = '\0';
+            }
+            else password.PasswordChar = '*'; 
+        }
+
+
+        //
+        // No important
+        //
+
+        private void panel_image1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
